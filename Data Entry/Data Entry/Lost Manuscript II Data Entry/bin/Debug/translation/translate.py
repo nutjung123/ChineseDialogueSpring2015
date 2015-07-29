87702fe28a5d11784d528c7b89ab3623b1a97ff9
ZishanHuang <zishan0huang@gmail.com>
2015-07-29 16:11:08 -0400

    Updated

--- Data Entry/Data Entry/Lost Manuscript II Data Entry/bin/Debug/translation/translate.py
+++ Data Entry/Data Entry/Lost Manuscript II Data Entry/bin/Debug/translation/translate.py
@@ -13,21 +13,22 @@ if(len(sys.argv) > 1):
     text_to_translate = sys.argv[1]
 else:
     print "No words translated"
-    exit()
+    #exit()
     
 #print text_to_translate
 #print translate(text_to_translate, 'en')
 #uni_text = unicode(text_to_translate)
-
+"""
 text_to_translate = text_to_translate.decode(sys.stdin.encoding or locale.getpreferredencoding(True)).encode('utf-8')
 #print text_to_translate
 print translate(text_to_translate,'en')
-
+"""
 
 #print type(text_to_translate)
 #print text_to_translate
 #print len(sys.argv)
-#print translate('bienvenue', 'zh-CN')
+#print translate('bienvenue','en')
+#print translate("Let's switch gears. The Olympic Green Tennis Center is the tennis center that hosted the tennis preliminaries and finals of singles and doubles for men and women at the 2008 Summer Olympics.", 'zh-CN')
 #print translate("我想知道更多有关网球的", "en")
 #print '哈哈。'
 #print '哈哈。'.decode('utf-8').encode('gb2312')
